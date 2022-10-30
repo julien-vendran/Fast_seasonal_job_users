3dad275411dfc7bf6de68f788747a69459239b1c
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2022-10-30 11:14:58 +0100

    feat(database): first implementation for user creation

--- src/main/java/com/polytech/users/UsersApplication.java
+++ src/main/java/com/polytech/users/UsersApplication.java
@@ -1,9 +1,11 @@
 package com.polytech.users;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class UsersApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/polytech/users/users/impl/UsersImpl.java
@@ -0,0 +1,11 @@
+package com.polytech.users.users.impl;
+
+import com.polytech.users.users.Users;
+import com.polytech.users.users.objects.UserInformation;
+
+public class UsersImpl implements Users {
+    @Override
+    public void create(UserInformation userInformation) {
+        //TODO: write data into the database
+    }
+}
