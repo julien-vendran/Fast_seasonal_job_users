c40b6b3a65dc2e33ebd4dace7f5500da31db65c6
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2022-11-10 14:44:51 +0100

    build: add postgres in docker-compose

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -3,11 +3,11 @@ CREATE DATABASE fast_seasonal;
 DROP TABLE IF EXISTS jobseeker;
 CREATE TABLE jobseeker
 (
-    id         serial PRIMARY KEY,
-    last_name  VARCHAR(100),
-    first_name VARCHAR(100),
-    city       VARCHAR(100),
-    job        VARCHAR(100),
-    phone      VARCHAR(20),
-    email      VARCHAR(100)
+    id        serial PRIMARY KEY,
+    lastname  VARCHAR(100),
+    firstname VARCHAR(100),
+    city      VARCHAR(100),
+    job       VARCHAR(100),
+    phone     VARCHAR(20),
+    email     VARCHAR(100)
 );
