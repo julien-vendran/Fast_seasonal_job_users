ddf38b0cfb5d815467083ad95469591237155a2e
Ophélie Amarine <ophelie.amarine@etu.umontpellier.fr>
2022-11-14 10:13:08 +0100

    refactor: delete useless files and refactor according to comments on PR

--- src/main/java/com/polytech/users/users/repository/RecruiterRepository.java
+++ src/main/java/com/polytech/users/users/repository/RecruiterRepository.java
@@ -5,5 +5,5 @@ import com.polytech.users.users.entity.RecruiterEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RecruiterRepository extends CrudRepository<RecruiterEntity, Long> {
-
+    public RecruiterEntity findByCompanyName(String companyName);
 }
