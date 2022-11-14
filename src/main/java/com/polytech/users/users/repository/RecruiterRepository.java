8945cc9028239f68dac03cc54079aef5b15cfc32
Ophélie Amarine <ophelie.amarine@etu.umontpellier.fr>
2022-11-14 10:44:31 +0100

    refactor: add annotations to controller and repository

--- src/main/java/com/polytech/users/users/repository/RecruiterRepository.java
+++ src/main/java/com/polytech/users/users/repository/RecruiterRepository.java
@@ -3,7 +3,9 @@ package com.polytech.users.users.repository;
 
 import com.polytech.users.users.entity.RecruiterEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecruiterRepository extends CrudRepository<RecruiterEntity, Long> {
     public RecruiterEntity findByCompanyName(String companyName);
 }
