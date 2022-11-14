5f06f6eff2c73a51b71dadbba970687d1fac42f8
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2022-11-14 09:13:30 +0100

    feat: configure api acccess and keycloack

--- Docker.md
+++ Docker.md
@@ -33,21 +33,5 @@ docker exec -it <container-name> bash
 # Keycloack 
 Lancer une instance de KeyCloack
 ```shell
-docker run -p 8443:8443 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:20.0.1 start-dev --hostname-port=8443
-
-docker run -p 3000:8443 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:20.0.1 start --hostname-port 3000
-        
-```## Postgres
-Pour avoir accès aux données, il faut créer une base de données postgresql dans docker
-```shell
-docker run --name postgres-db -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
-```
-Host: *localhost*  
-Port: *5432*  
-User: *postgres*  
-Password: *docker*
-
-Pour accéder au terminal du container depuis un terminal
-```shell
-docker exec -it <container-name> bash
+docker run -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:20.0.1 start-dev
 ```
--- src/main/java/com/polytech/users/config/SecurityConfig.java
+++ src/main/java/com/polytech/users/config/SecurityConfig.java
@@ -2,12 +2,17 @@ package com.polytech.users.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
@@ -34,4 +39,20 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/jobseeker*")
+            //.hasRole("USER")
+            //.anyRequest()
+            .permitAll();
+        return http.build();
+    }
+
 }
