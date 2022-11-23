cd564884f90e964d140231208c62479b8dad3f38
Ophélie Amarine <ophelie.amarine@etu.umontpellier.fr>
2022-11-23 16:28:55 +0100

    database: create table offer and jobseekerJoinOffer into local DB + schema

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -37,4 +37,34 @@ CREATE TABLE grade
     FOREIGN KEY (idJobseeker) REFERENCES jobseeker(id)
 );
 
+DROP TABLE IF EXISTS offer;
+CREATE TABLE offer
+(
+    idOffer serial PRIMARY KEY,
+    title  VARCHAR(100),
+    description VARCHAR(1000),
+    publishDay DATE,
+    jobStartingDate DATE,
+    location VARCHAR(100),
+    jobNumber INTEGER,
+    salary FLOAT,
+    advantages VARCHAR(500),
+    job VARCHAR(100),
+    zones VARCHAR(100),
+    author BIGINT,
+    keywords VARCHAR(100),
+    FOREIGN KEY (author) REFERENCES recruiter(id)
+);
+
+DROP TABLE IF EXISTS jobseekerJoinOffer;
+CREATE TABLE jobseekerJoinOffer
+(
+    idOffer BIGINT,
+    idJobseeker BIGINT,
+    PRIMARY KEY (idJobseeker, idOffer),
+    FOREIGN KEY (idJobseeker) REFERENCES jobseeker(id),
+    FOREIGN KEY (idOffer) REFERENCES offer(idOffer)
+
+);
+
 CREATE SEQUENCE hibernate_sequence START 1;
